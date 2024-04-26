1a107eedfd46e2d50e1735b9587b6563a12e7257
Yimix <60080905+unyime8@users.noreply.github.com>
2024-04-26 13:10:21 +0100

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-97e69ff2-293e-4395-afa6-0b691c39caba/"
 
 OUTPUT_DIR="logs"
 
