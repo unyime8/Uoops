cd535e84d401169d065e1d3dde3c9acb235987ff
Yimix <60080905+unyime8@users.noreply.github.com>
2024-04-26 13:16:11 +0100

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-97e69ff2-293e-4395-afa6-0b691c39caba/"
 
 OUTPUT_DIR="logs"
 
